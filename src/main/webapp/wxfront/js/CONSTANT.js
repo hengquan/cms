688e46e07367ea42b797f54741a2555fb7d0dd7e
wanghuiwhwx <wanghui_whwx@sina.com>
2017-09-27 09:45:06 +0800

    调整用户部分的前端

--- src/main/webapp/wxfront/js/CONSTANT.js
+++ src/main/webapp/wxfront/js/CONSTANT.js
@@ -1,4 +1,4 @@
 //var _URL_BASE="http://wl.weechao.com/wxmp.ql";
 //var _URL_BASE="http://crm.kylindpc.cn/wxmp.ql";
 //var _URL_BASE="http://localhost:8080/wxmp.ql";
-var _URL_BASE="http://localhost:8080/wxmp.ql";
+var _URL_BASE="http://localhost:5080/wxmp.ql";
