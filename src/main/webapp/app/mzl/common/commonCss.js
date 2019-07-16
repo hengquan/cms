4c7f39711d905d704a767d6cb0685d686704de1e
hengquan <1183998806@qq.com>
2019-07-16 13:47:45 +0800

    前端结构改变

---
+++ src/main/webapp/app/mzl/common/commonCss.js
@@ -0,0 +1,4 @@
+//引入统用CSS
+document.write('<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">');
+document.write('<link href="../layui-v2.5.4/layui/css/layui.css" rel="stylesheet">');
+document.write('<link href="../layui-v2.5.4/layui/css/modules/layer/default/layer.css" rel="stylesheet">');
---
+++ src/main/webapp/h5/mzl/common/commonCss.js
@@ -0,0 +1,2 @@
+//引入统用CSS
+document.write('<link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">');
